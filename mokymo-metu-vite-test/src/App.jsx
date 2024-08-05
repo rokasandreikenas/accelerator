94579c29c761e2303fd2c380fa56750539e3013f
rokasandreikenas <shyptt@gmail.com>
2024-08-05 20:23:02 +0300

    Add vite examples

---
+++ mokymo-metu-vite-test/src/App.jsx
@@ -0,0 +1,13 @@
+import SearchInput from "./components/SearchInput";
+import Topbar from "./components/Topbar";
+
+const App = () => {
+  return (
+    <div>
+      <Topbar />
+      <SearchInput />
+    </div>
+  );
+};
+
+export default App;
